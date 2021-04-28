174558f5cecdf21716b9d040671a793f56238003
Jared K <jaredkotoff@users.noreply.github.com>
2021-04-27 23:38:49 -0700

    feat: use go templating for cleaner html template replacment

--- main.go
+++ main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	_ "embed"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
-	"reflect"
-	"strconv"
 	"strings"
+	"text/template"
+	"time"
 
-	"github.com/sparkdemcisin81/promptui" // using this instead of manifoldco because of weird race conditions
+	"github.com/sparkdemcisin81/promptui" // using this instead of manifoldco because of weird bugs 🤷‍♂️
 )
 
-//go:embed template.html
+//go:embed template.gohtml
 var templateHtml string
 
 type UserAnswers struct {
@@ -28,26 +27,54 @@ type UserAnswers struct {
 	TransitionVideo     string
 }
 
-func main() {
-	outputHtmlName := "obs-random-videos.html"
-	audioFileExts := []string{".mp3", ".ogg", ".aac"}
-	videoFileExts := []string{".mp4", ".webm", ".mpeg4"}
-	mediaFileExts := append(audioFileExts, videoFileExts...)
+var outputHtmlName = "obs-random-videos.html"
+var audioFileExts = []string{".mp3", ".ogg", ".aac"}
+var videoFileExts = []string{".mp4", ".webm", ".mpeg4"}
+var mediaFileExts = append(audioFileExts, videoFileExts...)
+var promptDelay = 100 * time.Millisecond // helps with race conditionsin promptui
 
+func main() {
 	currentDir, err := os.Getwd()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("Failed to get current directory path: %v", err)
 	}
+	currentDir = strings.Replace(currentDir, "\\", "\\\\", -1) + "\\\\"
 	files, err := ioutil.ReadDir("./")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to read current directory: %v", err)
 	}
 	mediaFiles := filterFiles(files, mediaFileExts)
 	if len(mediaFiles) < 1 {
-		fmt.Printf("No media files found!")
+		log.Fatal("No media files found!")
 		return
 	}
+	outputHtml, err := os.Create(outputHtmlName)
+	if err != nil {
+		log.Fatalf("Failed create output file: %v", err)
+	}
 
+	answers, err := askQuestions(currentDir, mediaFiles)
+	if err != nil {
+		outputHtml.Close()
+		os.Remove(outputHtmlName)
+		log.Fatalf("Something went wrong getting user input: %v", err)
+	}
+	if answers.TransitionVideo != "" {
+		answers.MediaFiles = removeTransitionVideo(answers.TransitionVideo, answers.MediaFiles)
+	}
+
+	templateHtml = "<!--\nAUTO GENERATED FILE\nDON'T TOUCH\n-->\n" + templateHtml
+	t := template.Must(template.New("html").Funcs(template.FuncMap{"StringsJoin": strings.Join}).Parse(templateHtml))
+	err = t.Execute(outputHtml, answers)
+	if err != nil {
+		outputHtml.Close()
+		os.Remove(outputHtmlName)
+		log.Fatalf("Failed compiling template: %v", err)
+	}
+	outputHtml.Close()
+}
+
+func askQuestions(currentDir string, mediaFiles []string) (UserAnswers, error) {
 	answers := UserAnswers{
 		VideoFolder:         currentDir,
 		MediaFiles:          mediaFiles,
@@ -62,40 +89,26 @@ func main() {
 		answers.LoopFirstVideo = ShowQuestion("Do you want to loop the first video?", false)
 		answers.HaveTransitionVideo = ShowQuestion("Do have a transition video? (This video plays after every other video)", false)
 		if answers.HaveTransitionVideo {
+			err := errors.New("")
 			answers.TransitionVideo, err = GetTransitionVideo(mediaFiles)
-
 			if err != nil {
-				return
+				fmt.Printf("Prompt failed getting transition video: %v", err)
+				return answers, err
 			}
 		}
 	}
+	return answers, nil
+}
 
-	fields := reflect.TypeOf(answers)
-	values := reflect.ValueOf(answers)
-
-	for i := 0; i < fields.NumField(); i++ {
-		field := fields.Field(i)
-		value := values.Field(i)
-		finalValue := ""
-		switch value.Kind() {
-		case reflect.String:
-			finalValue = value.String()
-			if field.Name == "VideoFolder" {
-				finalValue = strings.Replace(finalValue, "\\", "\\\\", -1) + "\\\\"
-			}
-		case reflect.Bool:
-			finalValue = strconv.FormatBool(value.Bool())
-		case reflect.Slice:
-			j, _ := json.Marshal(value.Interface())
-			finalValue = string(j)
-		default:
-			finalValue = ""
+func removeTransitionVideo(transitionVideo string, mediaFiles []string) []string {
+	files := mediaFiles
+	for i, file := range mediaFiles {
+		if file == transitionVideo {
+			files[i] = files[len(files)-1]
+			return files[:len(files)-1]
 		}
-
-		templateHtml = strings.Replace(templateHtml, "$"+field.Name+"$", finalValue, 1)
 	}
-	templateHtml = "<!--\nAUTO GENERATED FILE\nDON'T TOUCH\n-->\n" + templateHtml
-	os.WriteFile(outputHtmlName, []byte(templateHtml), 0644)
+	return files
 }
 
 func filterFiles(files []fs.FileInfo, fileExts []string) []string {
@@ -121,9 +134,8 @@ func GetTransitionVideo(mediaFiles []string) (string, error) {
 		Items: items,
 	}
 	_, result, err := prompt.Run()
-
+	time.Sleep(promptDelay)
 	if err != nil {
-		fmt.Printf("Prompt failed getting transition video: %v\n", err)
 		return "", err
 	}
 	if strings.ToLower(result) != "cancel" {
@@ -156,10 +168,11 @@ func ShowQuestion(message string, defaultValueFlag bool) bool {
 
 	result, err := prompt.Run()
 	if err != nil {
+		time.Sleep(promptDelay)
 		return ShowQuestion(message, defaultValueFlag)
 	}
 
 	result = strings.ToLower(result)
-
+	time.Sleep(promptDelay)
 	return result == "y" || result == "yes"
 }
