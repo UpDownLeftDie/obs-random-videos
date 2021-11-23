13cd9f29a153bb1a1548fb31fbb1abca04525182
Jared Kotoff <jaredkotoff@users.noreply.github.com>
2021-11-23 13:21:52 -0800

    Persistent Playlist (#25)

--- js/body.js
+++ js/body.js
@@ -1,17 +1,44 @@
-const player = document.getElementById("videoPlayer1");
-const player2 = document.getElementById("videoPlayer2");
-player.addEventListener("ended", () => playNext(player2, player), {passive: true});
-player2.addEventListener("ended", () => playNext(player, player2), {passive: true});
+// @ts-check
+const player = /** @type {HTMLMediaElement} */ (
+  document.getElementById('videoPlayer1')
+);
 
-/* Initial logic */
+const player2 = /** @type {HTMLMediaElement} */ (
+  document.getElementById('videoPlayer2')
+);
+player.addEventListener(
+  'ended',
+  () => {
+    if (!loopFirstVideo) {
+      progressPlaylistState();
+    }
+    playNext(player2, player);
+  },
+  {
+    passive: true,
+  },
+);
+player2.addEventListener(
+  'ended',
+  () => {
+    if (!loopFirstVideo) {
+      progressPlaylistState();
+    }
+    playNext(player, player2);
+  },
+  {
+    passive: true,
+  },
+);
 
-const mp4Source = player.getElementsByClassName("mp4Source")[0];
-let video = `${mediaFolder}${mediaFiles[0]}`;
-// check if we played this video last run
-if (mediaFiles.length > 1 && localStorage.getItem("lastPlayed") === video) {
-    video = `${mediaFolder}${mediaFiles[1]}`;
-}
-mp4Source.setAttribute("src", video);
+/***** Initial load *****/
+
+const mp4Source = player.getElementsByClassName('mp4Source')[0];
+let video = getNextPlaylistItem();
+// have to move the state forward after getting the first video
+progressPlaylistState();
+
+mp4Source.setAttribute('src', video);
 player.load();
 
 playNext(player, player2);
--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-const mediaFolder = "{{ .MediaFolder }}";
-const initMediaFiles =  ["{{ StringsJoin .MediaFiles "\", \"" }}"];
-const transitionVideo = "{{ .TransitionVideo }}";
-const playOnlyOne = {{ .PlayOnlyOne }};
-const loopFirstVideo = {{ .LoopFirstVideo }};
+// @ts-check
+const mediaFolder = /** @type {string} */ ("{{ .MediaFolder }}");
+const initMediaFiles = /** @type {string[]} */ (["{{ StringsJoin .MediaFiles "\", \"" }}"]);
+const transitionVideo = /** @type {string} */("{{ .TransitionVideo }}");
+const playOnlyOne = /** @type {boolean} */ ({{ .PlayOnlyOne }});
+const loopFirstVideo = /** @type {boolean} */ ({{ .LoopFirstVideo }});
+const transitionVideoPath = /** @type {string} */ (
+  `${mediaFolder}${transitionVideo}`
+);
 
-const mediaFiles = shuffleArr(initMediaFiles);
-let count = 0;
 let isTransition = true;
 
+/**
+ * shuffleArr takes in an array and returns a new array in random order
+ * @param {any[]} a any array to be shuffled
+ * @return {any[]} shuffled array
+ */
 function shuffleArr(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
@@ -19,47 +26,134 @@ function shuffleArr(a) {
   return a;
 }
 
+/**
+ * prependFolderToFiles simply adds a folder path to a list of files and returns
+ *  the new array
+ * @param {string} folder folder path, must end with a trailing slash
+ * @param {string[]} files array of file names
+ * @returns {string[]} new array with full path to files
+ */
+function prependFolderToFiles(folder, files) {
+  return files.map((file) => `${folder}${file}`);
+}
+
+/**
+ * storePlaylistState takes in a playlist and stores it into localstorage
+ * @param {string[]} state
+ * @returns {void}
+ */
+function storePlaylistState(state) {
+  localStorage.setItem('playlist', JSON.stringify(state));
+}
+
+/**
+ * getNewPlaylist creates a newly randomize list of files and stores it in
+ *  localstorage
+ * @returns {string[]} a new playlist
+ */
+function getNewPlaylist() {
+  const playlist = prependFolderToFiles(
+    mediaFolder,
+    shuffleArr(initMediaFiles),
+  );
+  storePlaylistState(playlist);
+  return playlist;
+}
+
+/**
+ * getPlaylist will get the playlist state form localstorage or create a new one
+ * @returns {string[]} current playlist state
+ */
+function getPlaylist() {
+  let playlist = [];
+  try {
+    playlist = JSON.parse(localStorage.getItem('playlist'));
+  } catch {
+    console.log('playlist empty!');
+  }
+  if (!playlist || playlist.length === 0) {
+    playlist = getNewPlaylist();
+  }
+  return playlist;
+}
+
+/**
+ * progressPlaylistState removes the last item from the playlist and stores the
+ *  updated version in localstorage
+ * @returns {void}
+ */
+function progressPlaylistState() {
+  const playlist = getPlaylist();
+  playlist.pop();
+  storePlaylistState(playlist);
+}
+
+/**
+ * getNextPlaylistItem returns the next item in the playlist unless it matches
+ *  the last thing played then it moves that item to the end and returns the
+ *  next item after that
+ * @returns {string} the next item in the playlist
+ */
+function getNextPlaylistItem() {
+  const playlist = getPlaylist();
+  let mediaItem = playlist.pop();
+
+  // check if we played this mediaItem last run
+  console.log({ lastPlayed: localStorage.getItem('lastPlayed'), mediaItem });
+  if (localStorage.getItem('lastPlayed') === mediaItem) {
+    // moves the repeated item to the end so its not skipped entirely
+    storePlaylistState([mediaItem].concat(playlist));
+    mediaItem = getNextPlaylistItem();
+  }
+  return mediaItem;
+}
+
+/**
+ * playNext is the core function of this project and handles the loading and
+ *   playing of the alternating video players
+ * @param {HTMLMediaElement} player currently playing video player
+ * @param {HTMLMediaElement} nextPlayer the next video player to be played
+ * @returns {void}
+ */
 function playNext(player, nextPlayer) {
-  const nextMp4Source = nextPlayer.getElementsByClassName("mp4Source")[0];
   const currentMp4Source = player.getElementsByClassName('mp4Source')[0];
-  if(!loopFirstVideo) {
-    if (!transitionVideo || !isTransition) {
-      count++;
-      if (count > mediaFiles.length - 1) count = 0;
-    }
+  const nextMp4Source = nextPlayer.getElementsByClassName('mp4Source')[0];
+  const currentVideo = currentMp4Source.getAttribute('src');
+  if (currentVideo !== transitionVideoPath) {
+    localStorage.setItem('lastPlayed', currentVideo);
   }
 
+  let video = localStorage.getItem('lastPlayed');
+  if (!loopFirstVideo && (!transitionVideo || !isTransition)) {
+    video = getNextPlaylistItem();
+    console.log(`next video: ${video}`);
+  }
+
+  // TODO: we can use this opacity to crossfade between mediaFiles
+  player.style['z-index'] = 1;
+  player.style['opacity'] = '1';
+  nextPlayer.style['z-index'] = 0;
+  nextPlayer.style['opacity'] = '0';
+
+  if (transitionVideo && transitionVideo !== '' && isTransition) {
+    video = transitionVideoPath;
+    isTransition = false;
+  } else {
+    isTransition = true;
+  }
+  nextMp4Source.setAttribute('src', video);
+  nextPlayer.load();
+  nextPlayer.pause();
+
   if (playOnlyOne) {
-    if (count > 1) {
-      // Remove video after playing once
+    // Remove videos after playing once
+    player.onended = () => {
       currentMp4Source.removeAttribute('src');
       nextMp4Source.removeAttribute('src');
       player.load();
       nextPlayer.load();
-    }
-  } else {
-    // TODO: we can use this opacity to crossfade between mediaFiles
-    player.style["z-index"] = 1;
-    player.style["opacity"] = 1;
-    nextPlayer.style["z-index"] = 0;
-    nextPlayer.style["opacity"] = 0;
-    let video = `${mediaFolder}${mediaFiles[count]}`;
-
-    const transitionVideoPath =  `${mediaFolder}${transitionVideo}`
-    if (transitionVideo && transitionVideo !== "" && isTransition) {
-      video = transitionVideoPath;
-      isTransition = false;
-    } else {
-      isTransition = true;
-    }
-    nextMp4Source.setAttribute("src", video);
-    nextPlayer.load();
-    nextPlayer.pause();
-
-    const currentVideo = currentMp4Source.getAttribute("src");
-    if (currentVideo !== transitionVideoPath) {
-      localStorage.setItem("lastPlayed", currentVideo);
-    }
-    player.play();
+    };
   }
+
+  player.play();
 }
